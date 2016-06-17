87fc7adff326f24273321e2c5d703efec7399a12
wcrowe <crowe.will@gmail.com>
2016-06-17 10:03:18 -0400

    Last night

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,42 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import { Link } from 'react-router';
+import { TextAlignment } from 'react-foundation-components/lib/text-alignment';
+import { Row, Column } from 'react-foundation-components/lib/grid';
 
-
-export default function Examples(props, context) {
+export default function Examples() {
   return (
-    <h2>
-      Examples
-    </h2>
+    <div>
+      <TextAlignment alignment="center">
+
+        <h2>
+          Examples
+        </h2>
+        <p>Here are some examples:</p>
+        <Row>
+          <Column largeCentered="centered" small={4}>
+            <ol>
+              <Row>
+                <Column largeCentered="centered" small={4}>
+                  <TextAlignment alignment="left">
+                    <li>
+                      <Link to="/?location=Tampa">Tampa, FL</Link>
+                    </li>
+                  </TextAlignment>
+                </Column>
+              </Row>
+              <Row>
+                <Column largeCentered="centered" small={4}>
+                  <TextAlignment alignment="left">
+                    <li>
+                      <Link to="/?location=Boulder">Boulder, CO</Link>
+                    </li>
+                  </TextAlignment>
+                </Column>
+              </Row>
+            </ol>
+          </Column>
+        </Row>
+      </TextAlignment>
+    </div>
   );
 }
-
