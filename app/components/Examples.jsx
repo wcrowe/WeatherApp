e0b1e88d3dd2206ab55ade7cad68233eaf7d95e2
wcrowe <crowe.will@gmail.com>
2016-11-02 15:52:01 -0400

    Checking In

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,47 +1,59 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { Grid, Row, Cell } from 'react-inline-grid';
+import { Flex, Box } from 'reflexbox';
 
 export default function Examples() {
   return (
     <div>
-      <Grid>
-        <Row is="center">
-          <Cell is="2">
-            <h2>
-              Examples
-            </h2>
-          </Cell>
-        </Row>
-      </Grid>
-      <Grid>
-        <Row is="center">
-          <Cell is="2">
+      <Flex
+        align="center"
+        gutter={2}
+        justify="center"
+      >
+        <Box
+          justify="center"
+          align="center"
+          col={4}
+          p={2}
+        >
+          <h2>
+            Examples
+          </h2>
+        </Box>
+      </Flex>
+      <Flex
+        align="center"
+        gutter={2}
+        justify="center"
+      >
+          <Box
+            justify="center"
+            align="center"
+            col={4}
+          >
             <p>Here are some examples:</p>
-          </Cell>
-        </Row>
-      </Grid>
-      <ol>
-        <Grid>
-          <Row is="center">
-            <Cell is="2">
+          </Box>
+          <ol>
+            <Box
+              col={12}
+              p={2}
+            >
               <li>
                 <Link to="/?location=Tampa">Tampa, FL</Link>
-              </li>
-            </Cell>
-          </Row>
-        </Grid>
-        <Grid>
-          <Row is="center">
-            <Cell is="2">
+             </li>
+          </Box>
+            <Box
+              col={12}
+              p={2}
+            >
               <li>
                 <Link to="/?location=Boulder">Boulder, CO</Link>
               </li>
-            </Cell>
-          </Row>
-        </Grid>
-      </ol>
+            </Box>
 
+          </ol>
+      </Flex>
     </div>
   );
-}
+  }
